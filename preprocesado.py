3c076b79a72b7cb9416f6b05de5a436b6f399a8f
vclar <violeta.clar@opendeusto.es>
2024-02-15 16:26:48 +0100

    nombres

--- preprocesado.py
+++ preprocesado.py
@@ -2,7 +2,7 @@ import csv
 import os
 import itertools
 
-def procesar_y_escribir_linea(ruta_entrada, ruta_salida, num_lineas=100000):
+def procesar_y_escribir_linea(ruta_entrada, ruta_salida, num_lineas):
     with open(ruta_entrada, newline='') as entrada, open(ruta_salida, 'w', newline='') as salida:
         lector_csv = csv.reader(itertools.islice(entrada, num_lineas))
         escritor_csv = csv.writer(salida)
@@ -20,4 +20,4 @@ def procesar_y_escribir_linea(ruta_entrada, ruta_salida, num_lineas=100000):
 nombre_archivo_entrada = "/home/laboratorio-ss-03/Escritorio/completo.csv"
 nombre_archivo_salida = "/home/laboratorio-ss-03/Escritorio/procesado.csv"
 
-procesar_y_escribir_linea(nombre_archivo_entrada, nombre_archivo_salida, num_lineas=100000)
+procesar_y_escribir_linea(nombre_archivo_entrada, nombre_archivo_salida, 200000)
